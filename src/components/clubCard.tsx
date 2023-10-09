e322ada881238fbc9fa5056ccaf868216f2096f3
Amaya Lucas <amayajlucas@gmail.com>
2023-10-08 22:31:19 -0400

    club cards previewing first book in the reading list

--- src/components/clubCard.tsx
+++ src/components/clubCard.tsx
@@ -6,7 +6,11 @@ const ClubCard: React.FC = ({card}) => {
   return (
     // Add router link to IonCard later to open it up and view card
     <IonCard routerLink={`/club/id/${card.id}`} routerDirection='forward'>
-      <img alt='' src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      {card.books.find(Boolean) ? (
+        <img alt='' src={card.books.find(Boolean).data.volumeInfo.imageLinks.medium} />
+      ) : (
+        <img alt='' src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      )}
       <IonCardHeader>
         <IonCardTitle>{card.clubName}</IonCardTitle>
       </IonCardHeader>
